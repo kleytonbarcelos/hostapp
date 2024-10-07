b434ebf9c000391529b603b4e4f36e42d2c1918f
kleytonbarcelos <kleytonbarcelos@gmail.com>
2024-10-07 13:44:06 -0300

    update2

--- app/middleware.js
+++ app/middleware.js
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const host = req.headers.get("host") || "Host não identificado";
+  const host = req.nextUrl.host || "Host não identificado";
 
-  // Adiciona o host no cabeçalho para ser utilizado no componente da página
   const response = NextResponse.next();
   response.headers.set("x-host", host);
   return response;
